bbd8e83dc3bdbbe329544649dc805120db88d5ca
MBriese <mindi.briese@gmail.com>
2025-02-19 13:38:32 -0800

    setup API - error still happening

--- issue-tracker/src/app/api/issues/route.ts
+++ issue-tracker/src/app/api/issues/route.ts
@@ -6,19 +6,29 @@ const prisma = new PrismaClient();
 const createIssueSchema
     = z.object({
     title: z.string().min(1).max(255),
-    description: z.string().min(1)
+    description: z.string().min(1),
+    updatedAt: z.date().optional(),
+    createdAt: z.date().optional(),
 });
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
     const validation = createIssueSchema.safeParse(body);
-    if (!validation.success)
-        return NextResponse.json(validation.error.errors, {status:400});
-    const [newIssue] = await Promise.all([prisma.issue.create({
+    if (!validation.success) {
+        return NextResponse.json(validation.error.errors, {status: 400});
+    }
+
+    const {title, description } = validation.data;
+    const updatedAt = new Date();
+    const createdAt = new Date();
+
+    const newIssue = await prisma.issue.create({
         data: {
-            title: body.title,
-            description: body.description,
+            title,
+            description,
+            updatedAt: updatedAt || new Date(),
+            createdAt: createdAt || new Date(),
         }
-    })])
+    });
     return NextResponse.json(newIssue, { status: 201} );
 }
